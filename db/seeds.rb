d9b6e0f8474dc946aca18b76ef89e7cfec229c7c
Yasna Hormazabal <yasnahormazabalc@gmail.com>
2021-11-18 13:56:03 -0300

    correction load items seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+    first_name: "Pedro",
+    last_name: "Perez",
+    email_address: "pedro.perez@hotmail.com",
+    age: "25"
+)
+User.create(
+    first_name: "Luis",
+    last_name: "Martinez",
+    email_address: "luis.martinez@hotmail.com",
+    age: "17"
+)
+User.create(
+    first_name: "Maria",
+    last_name: "Ponce",
+    email_address: "maria.ponce@hotmail.com",
+    age: "40"
+)
