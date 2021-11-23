7a692e145711b277b84c98dffaf6356271a3c3a1
Yasna Hormazabal <yasnahormazabalc@gmail.com>
2021-11-23 12:24:24 -0300

    proyecto terminado

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :first_name, :last_name, :email_address, :age, presence: true
+    validates :age, numericality: {only_integer: true}
+    validates :first_name, :last_name, length: {minimum: 2}
+    validates_numericality_of :age, less_than: 150
+    validates_numericality_of :age, greater_than: 10
 end
